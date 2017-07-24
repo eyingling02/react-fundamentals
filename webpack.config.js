e3b3c56dbe06c180f7563e2764932ee89bd30bf0
eyingling02 <eyingling02@gmail.com>
2017-07-24 16:55:16 -0700

    Finish popular module

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,13 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(js)$/, exclude: /node_modules/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
+      template: './app/index.html'
     })
   ]
 };
