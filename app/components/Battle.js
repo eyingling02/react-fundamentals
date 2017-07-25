8720facc604d75a16394143c52119a533c4e06fb
eyingling02 <eyingling02@gmail.com>
2017-07-24 23:38:05 -0700

    Add upto forms part

---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>BATTLE!</p>
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
